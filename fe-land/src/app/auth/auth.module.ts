87b4ece304f0c60db90b7dae58cc7b838771cd66
shyclown <roman.moravcik1@gmail.com>
2018-10-21 23:32:06 +0200

    init src

--- fe-land/src/app/auth/auth.module.ts
+++ fe-land/src/app/auth/auth.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {SignInFormComponent} from "./sign-in-form/sign-in-form.component";
+import {LoginFormComponent} from "./login-form/login-form.component";
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+
+  exports:[
+      SignInFormComponent,
+      LoginFormComponent
+  ],
+
+  declarations: [
+      SignInFormComponent,
+      LoginFormComponent
+  ]
 })
 export class AuthModule { }
